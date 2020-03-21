19064f7e22e342ed4c4659f03ef873fa2b6ce713
Franklin <franklin.bdantas@hotmail.com>
2020-03-21 16:45:53 -0300

    TilePhysics.ApplyForce now have a Vector2 param (direction)

--- TiMovi/TilePhysics.cs
+++ TiMovi/TilePhysics.cs
@@ -1,11 +1,12 @@
-﻿using UnityEngine;
+﻿using Extensions.Unity;
+using UnityEngine;
 
 namespace TiMovi
 {
     /// <summary>
     /// Defines a class to control TilePhysics 
     /// </summary>
-    public static class TilePhysics 
+    public static class TilePhysics
     {
         /// <summary>
         /// Changes a coordinate to a especific force in a certain direction
@@ -15,6 +16,7 @@ namespace TiMovi
         /// <param name="vertical">Defines if the movement is vertical</param>
         /// <param name="force">Define the amount of the tiles the Eneity will move (use negative values to back)</param>
         /// <returns>Returns the new coordinate result of the movement</returns>
+        [System.Obsolete("Use this method with the Vector2 params (direction)")]
         public static Coordinate ApplyForce(Coordinate coordinate, Coordinate maxCoordinates, bool vertical, int force = 1)
         {
             if (!vertical)
@@ -40,7 +42,7 @@ namespace TiMovi
 
                 VerifyLimits(ref y, ref Y, my, mY);
 
-                coordinate.Local.Set(coordinate.Local.x,y);
+                coordinate.Local.Set(coordinate.Local.x, y);
                 coordinate.Global.Set(coordinate.Global.x, Y);
             }
             return coordinate;
@@ -53,32 +55,64 @@ namespace TiMovi
         /// <param name="V">Could be global x or the global y of some coordinate</param>
         /// <param name="mv">Could be max local x or the max local y of some coordinate</param>
         /// <param name="mV">Could be max global x or the max global y of some coordinate</param>
-        private static void VerifyLimits(ref int v, ref int V, int mv,int mV)
+        private static void VerifyLimits(ref int v, ref int V, int mv, int mV)
         {
             if (v > mv)
             {
                 v = v - mv;
                 V++;
-                V = V > mV? mV : V;
+                V = V > mV ? mV : V;
             }
 
-            if(v < 0)
+            if (v < 0)
             {
                 v = mv - System.Math.Abs(v);
                 V = V < 0 ? 0 : V;
             }
 
         }
-
-        public static Coordinate ApplyForce(Coordinate coordinate, Coordinate maxCoordinates,Vector2 direction)
+        /// <summary>
+        /// Changes a coordinate to a especific force in a certain direction
+        /// </summary>
+        /// <param name="coordinate">The actual coordinate of an Entity</param>
+        /// <param name="maxCoordinates">The max coordinates of the map</param>
+        /// <param name="direction">Defines the direction of the Entity will move</param>
+        /// <returns>Returns the new coordinate result of the movement</returns>
+        public static Coordinate ApplyForce(Coordinate coordinate, Coordinate maxCoordinates, Vector2 direction)
         {
             coordinate.Local += direction;
-            /*if(coordinate.Local > maxCoordinates.Local)
-            {
-                Probably needs a new extension method
-            }*/
-            return null;
-        }
 
+            while (coordinate.Local.GreaterThan(maxCoordinates.Local))
+            {
+                if (coordinate.Local.x > maxCoordinates.Local.x || coordinate.Local.x < 0)
+                {
+                    int x = (int)coordinate.Local.x;
+                    int X = (int)coordinate.Global.x;
+
+                    int mx = (int)maxCoordinates.Local.x;
+                    int mX = (int)maxCoordinates.Global.x;
+
+                    VerifyLimits(ref x, ref X, mx, mX);
+
+                    coordinate.Local.Set(x, coordinate.Local.y);
+                    coordinate.Global.Set(X, coordinate.Global.y);
+                }
+                if (coordinate.Local.y > maxCoordinates.Local.y || coordinate.Local.y < 0)
+                {
+                    int y = (int)coordinate.Local.y;
+                    int Y = (int)coordinate.Global.y;
+
+                    int my = (int)maxCoordinates.Local.y;
+                    int mY = (int)maxCoordinates.Global.y;
+
+                    VerifyLimits(ref y, ref Y, my, mY);
+
+                    coordinate.Local.Set(coordinate.Local.x, y);
+                    coordinate.Global.Set(coordinate.Global.x, Y);
+                }
+            }
+
+            return coordinate;
+        }
     }
 }
