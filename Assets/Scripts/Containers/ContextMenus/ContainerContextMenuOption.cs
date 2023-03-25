79a28ea34fc3d2c65db90c7daf05f052a493049d
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-03-25 16:26:39 -0300

    Context Menu Layout started

---
+++ Assets/Scripts/Containers/ContextMenus/ContainerContextMenuOption.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace TheChest.ContextMenus
+{
+    [Serializable]
+    public class ContainerContextMenuOption
+    {
+        [SerializeField] protected string title;
+        [SerializeField] protected Image image;
+        [SerializeField] protected UnityEvent action;
+    
+        public string Title => this.title;
+        public Image Image => this.image;
+    }          
+}              
