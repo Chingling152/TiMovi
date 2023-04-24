eca53774dbb257ce3e7c1dda69c22a4a2cc58dfa
Franklin Bitencourt Dantas <franklin.bdantas@hotmail.com>
2023-04-23 21:00:22 -0300

    Fix project folder architecture

---
+++ Assets/Scripts/Examples/Containers/Container.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using TheChest.Containers;
+using TheChest.Containers.Generics;
+using TheChest.Containers.Generics.Base;
+using TheChest.Items;
+
+namespace TheChest.Examples.Containers
+{
+    public class Container : BaseContainer<Item>
+    {
+        [SerializeField]
+        protected Slot[] slots;
+
+        public override ISlot<Item>[] Slots 
+        { 
+            get => slots; 
+            protected set => slots = value as Slot[]; 
+        }
+    }
+}
